0b0bd4af56c260fcc88d5eb73d582de5588f16f9
vilson_lu@dlsu.ph <vilson_lu@dlsu.ph@caba358b-0947-af2e-156c-824192d2fdc2>
2013-08-23 10:52:03 +0000

    Inayos ko lang yung pag get ng password.

---
+++ DESIPAT-Phase3-Test1/src/main/LoginModel.java
@@ -0,0 +1,78 @@
+package src.main;
+
+import java.sql.SQLException;
+
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import src.DAO.UserDAO;
+import Model.UserModel;
+
+/**
+ * MODEL for Login
+ * Error checking
+ */
+public class LoginModel {
+	private UserDAO udao = UserDAO.getInstance();
+	private UserModel umdl;
+	
+	/**
+	 * Checks if username field is empty
+	 * @param username
+	 * @return TRUE if username field is not empty
+	 */
+	public boolean checkUsernameField(JTextField username) {
+		if(username.getText().isEmpty())
+			return false;
+		else
+			return true;
+	}
+	
+	/**
+	 * Checks if password field is empty
+	 * @param password
+	 * @return TRUE if password field is not empty
+	 */
+	public boolean checkPasswordField(JPasswordField password) {
+                String pass = new String(password.getPassword());
+		if(pass.equals(""))
+			return false;
+		else
+			return true;
+	}
+	
+	/**
+	 * This checks is the username matches the password
+	 * @param umdl
+	 * @param username
+	 * @param password
+	 * @return TRUE if username matches password, else returns false
+	 * @throws SQLException
+	 */
+	public boolean checkMatch(UserModel umdl, JTextField username, JPasswordField password) throws SQLException {
+		try {
+			if (umdl.getUsername().equals(username.getText()) && umdl.getPassword().equals(password.getText())) {
+				return true;
+			}
+		} catch (Exception e) {
+			
+		}
+		return false;
+	}
+
+	public String getUser(String username) throws SQLException {
+		return udao.getUser(username).getUsername();
+	}
+	
+	/*
+	 * GET COMPONENTS
+	 */
+	public UserModel getUserDAO(JTextField username) throws SQLException {
+		umdl = udao.getUser(username.getText());
+		return umdl;
+	}
+	
+	public UserModel getUserMdl() {
+		return umdl;
+	}
+}
